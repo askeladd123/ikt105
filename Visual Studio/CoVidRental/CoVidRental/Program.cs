8a8387acfd72200487242e636f24dda144abea58
Ask Sødal <asks@uia.no>
2022-05-25 02:37:53 +0200

    yoyoyo

--- Visual Studio/CoVidRental/CoVidRental/Program.cs
+++ Visual Studio/CoVidRental/CoVidRental/Program.cs
@@ -5,7 +5,7 @@ Console.WriteLine(
     "Requirements: this program expects a running database,\n" +
     "\tthat must be created by the \"database_setup.txt\" file. \n");
 
-Console.WriteLine("Loading scripts...");
+Console.WriteLine("Loading scripts:");
 
 // lese scipts
 Dictionary<string, string> scripts = new Dictionary<string, string>();
@@ -14,18 +14,18 @@ try
     scripts.Add("sparringer_populate.txt", System.IO.File.ReadAllText("sparringer_populate.txt"));
     scripts.Add("sparringer_branch.txt", System.IO.File.ReadAllText("sparringer_branch.txt"));
     scripts.Add("sparringer_business.txt", System.IO.File.ReadAllText("sparringer_business.txt"));
-    Console.WriteLine("Scipts loaded. \n");
+    Console.WriteLine("\tscipts loaded. \n");
 }
 catch (Exception e)
 {
-    Console.WriteLine("Couldn't load scipts: {0}", e.Message);
+    Console.WriteLine("couldn't load scipts: {0}", e.Message);
     Console.WriteLine("Press a key to quit. ");
     Console.ReadLine();
     Environment.Exit(-1);
 }
 
 Console.WriteLine("Enter connection data, example: \"server=localhost;userid=root;password=lol123;database=mydb\"\n" +
-    "\tenter \"d\" for default data: ");
+    "enter \"d\" for default data: ");
 string connection_data = Console.ReadLine();
 
 // koble til server
@@ -39,19 +39,21 @@ try
 {
     connection = new MySqlConnection(connection_data);
     connection.Open();
-    Console.WriteLine("Connection successful. \n");
+    Console.WriteLine("\tconnection successful. \n");
 }
 catch (MySqlException e)
 { 
-    Console.WriteLine("Connection failed: {0}", e.Message);
+    Console.WriteLine("connection failed: {0}", e.Message);
     Console.WriteLine("Press a key to quit. ");
     Console.ReadLine();
     Environment.Exit(-1);
 }
 
+Console.WriteLine(" - - - - - - - - - - - -\n");
+
 foreach (KeyValuePair<string, string> pair in scripts)
 {
-    Console.WriteLine("Executing script {0}", pair.Key);
+    Console.WriteLine("Executing script {0}\n", pair.Key);
     string[] queries = pair.Value.Split(';', StringSplitOptions.RemoveEmptyEntries);
 
     // kjør alle spørriger
@@ -69,10 +71,10 @@ foreach (KeyValuePair<string, string> pair in scripts)
             // alle SELECT spørringer
             MySqlDataReader data_reader = command.ExecuteReader();
 
-            Console.WriteLine("Query successful. Data recieved: ");
+            Console.WriteLine("\tQuery successful. Data recieved: ");
             while (data_reader.Read())
             {
-                Console.Write(" * ");
+                Console.Write("\t* ");
                 int i = 0;
                 for (; i < data_reader.FieldCount - 1; i++)
                 {
@@ -90,7 +92,7 @@ foreach (KeyValuePair<string, string> pair in scripts)
             try
             {
                 command.ExecuteNonQuery();
-                Console.WriteLine("Query successful. \n");
+                Console.WriteLine("\tQuery successful. \n");
             }
             catch (MySqlException e)
             {
@@ -99,7 +101,7 @@ foreach (KeyValuePair<string, string> pair in scripts)
         }
     }
 
-    Console.WriteLine("Executed script {0}", pair.Key);
+    Console.WriteLine("Executed script {0}\n\n - - - - - - - - - - - -\n", pair.Key);
 }
 
 connection.Close();
